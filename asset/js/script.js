d4d88fab3fb7e2dd1033498b588f664a1a362e9f
Steph-en <appahstephen9@gmail.com>
2021-07-13 23:51:02 +0000

    done with entire project

---
+++ asset/js/script.js
@@ -0,0 +1,10 @@
+const lables = document.querySelectorAll(
+    '.form-control label'
+)
+
+lables.forEach(lable => {
+    lable.innerHTML = lable.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 80}ms">${letter}</span>`)
+        .join('')
+})
